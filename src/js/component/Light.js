764f1e7c31ee1bfe21672558ea890f2c1a86a508
Jizga <jizga93@gmail.com>
2021-06-12 21:16:49 +0000

    Light component changes to color when you click it

--- src/js/component/Light.js
+++ src/js/component/Light.js
@@ -4,11 +4,15 @@ import PropTypes from "prop-types";
 export function Light(props) {
 	return (
 		<div
+			id="light"
 			className="p-5 w-50 rounded-circle mt-2 ml-5 mb-2"
-			style={{ backgroundColor: props.color }}></div>
+			style={{ backgroundColor: props.color }}>
+			{/* onClick={() => props.changeColor}> */}
+		</div>
 	);
 }
 
 Light.propTypes = {
 	color: PropTypes.string
+	// changeColor: PropTypes.func
 };
--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Light } from "./Light";
 
 export function TrafficLight() {
+	const [redColor, setRedColor] = useState("rgb(153, 10, 10)");
+	const [yellowColor, setYellowColor] = useState("rgb(226, 187, 14)");
+	const [greenColor, setGreenColor] = useState("rgb(2, 97, 2)");
+
+	function changeColor(colorP) {
+		if (colorP === "rgb(153, 10, 10)") setRedColor("rgb(255, 5, 5)");
+		if (colorP === "rgb(226, 187, 14)") setYellowColor("rgb(248, 252, 5)");
+		if (colorP === "rgb(2, 97, 2)") setGreenColor("rgb(3, 250, 3)");
+	}
+
 	return (
 		<div className="container d-flex justify-content-center mt-5">
 			<div className="row mt-5 w-25 pt-4 pb-4 shadow bg-dark rounded">
 				<div className="col-12 pl-4 pr-0">
-					{/* RED */}
-					<Light color={"red"} />
-					{/* YELLOW */}
-					<Light color={"yellow"} />
-					{/* GREEN */}
-					<Light color={"green"} />
+					<div onClick={() => changeColor(redColor)}>
+						<Light
+							color={redColor}
+							// changeColor={changeColor()}
+							// onClick={() => changeColor()}
+						/>
+					</div>
+
+					<div onClick={() => changeColor(yellowColor)}>
+						<Light
+							color={yellowColor}
+							// onClick={() => changeColor()}
+						/>
+					</div>
+
+					<div onClick={() => changeColor(greenColor)}>
+						<Light
+							color={greenColor}
+							// changeColor={changeColor()}
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
